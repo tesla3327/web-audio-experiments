1c4708d87548bf2c8a15c318b8442437494e12f7
Michael Thiessen <michael.thiessen@vidyard.com>
2018-06-06 15:39:06 -0400

    Added gate that inserts whitenoise into Sandstorm

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ audioTag.addEventListener('canplaythrough', () => {
     fetch(url)
       .then(resp => resp.arrayBuffer())
       .then(buffer => offlineCtx.decodeAudioData(buffer))
-      .then(insertWhiteNoise)
+      // .then(calculateRMS)
+      .then(gateWithWhiteNoise)
+      // .then(insertWhiteNoise)
       .then(connectToAudioTag);
   }
 });
@@ -58,10 +60,117 @@ const generateWhiteNoise = length => {
   const data = new Float32Array(length);
 
   for (let i = 0; i < length; i++) {
-    data[i] = (Math.random() * 2.0) - 1.0;
+    data[i] = ((Math.random() * 2.0) - 1.0) / 8;
   }
 
   return data;
+};
+
+/**
+ * @param {ArrayBuffer} buffer 
+ * @param {int} chunkSize 
+ */
+const getRMSChunks = (buffer, chunkSize=1024) => {
+  const rmsChunks = [];
+
+  let pos = 0;
+  let chunk;
+  let sum;
+  while (pos + chunkSize < buffer.length) {
+    sum = 0;
+    chunk = buffer.slice(pos, pos + chunkSize);
+
+    for (let i = 0; i < chunkSize; i++) {
+      sum += buffer[pos + i] * buffer[pos + i];
+    }
+    
+    rmsChunks.push(Math.sqrt(sum / chunkSize));
+    pos += chunkSize;
+  }
+
+  return rmsChunks;
+};
+
+const gateWithWhiteNoise = buffer => {
+  const t0 = performance.now();
+
+  const data = [buffer.getChannelData(0), buffer.getChannelData(1)];
+  const newData = [
+    new Float32Array(buffer.length),
+    new Float32Array(buffer.length)
+  ];
+
+  // Insert whitenoise when the level drops below a threshold level
+  const threshold = 0.15;
+  const chunkSize = 1024;
+  // Number of chunks in a row that need to be below threshold for it to trigger
+  const triggerLength = 10;
+  // Number of chunks to close the gate for once it's triggered
+  const holdLength = 10;
+  const silence = (new Float32Array(chunkSize)).fill(0);
+  const rmsChunks = getRMSChunks(data[0], chunkSize);
+
+  const totalAverage = rmsChunks.reduce((prev, next) => prev + next, 0) / rmsChunks.length;
+  console.log("Total track RMS: ", totalAverage);
+
+  let pos = 0;
+  let chunkPos = 0;
+  let trigger;
+
+  while (chunkPos < rmsChunks.length - holdLength) {
+    trigger = false;
+    const chunk = rmsChunks[chunkPos];
+  
+    // Check if we trigger the noise gate closed
+    if (chunk < threshold) {
+      trigger = true;
+      // Check if the next several chunks also fall below threshold
+      for (let i = 1; i <= triggerLength; i++) {
+        if (rmsChunks[chunkPos + i] >= threshold) {
+          trigger = false;
+        }
+      }
+    }
+
+    if (trigger) {
+      for (let i = 0; i < holdLength; i++) {
+        const whiteNoise = generateWhiteNoise(chunkSize);
+        newData[0].set(whiteNoise, pos);
+        newData[1].set(whiteNoise, pos);
+        pos += chunkSize;
+        chunkPos++;
+      }
+    } else {
+      newData[0].set(data[0].slice(pos, pos + chunkSize), pos);
+      newData[1].set(data[1].slice(pos, pos + chunkSize), pos);
+      pos += chunkSize;
+      chunkPos++;
+    }
+  }
+
+  const newBuffer = new AudioBuffer({
+    length: buffer.length,
+    numberOfChannels: buffer.numberOfChannels,
+    sampleRate: buffer.sampleRate
+  });
+
+  newBuffer.copyToChannel(newData[0], 0);
+  newBuffer.copyToChannel(newData[1], 1);
+
+  const t1 = performance.now();
+  console.log('Processed (Gate) in:', t1 - t0);
+
+  return Promise.resolve(newBuffer);
+};
+
+const calculateRMS = buffer => {
+  const rmsChunks = getRMSChunks(buffer.getChannelData(0));
+  const totalAverage = rmsChunks.reduce((prev, next) => prev + next, 0) / rmsChunks.length;
+
+  console.log("Total track RMS: ", totalAverage);
+  rmsChunks.forEach((chunk, i) => console.log(i + ' : ' + Math.round(chunk * 100)));
+
+  return Promise.resolve(buffer);
 };
 
 const insertWhiteNoise = buffer => {
@@ -105,7 +214,7 @@ const insertWhiteNoise = buffer => {
   newBuffer.copyToChannel(newData[1], 1);
 
   const t1 = performance.now();
-  console.log('Timing:', t1 - t0);
+  console.log('Processed (InsertWhiteNoise) in:', t1 - t0);
 
   return Promise.resolve(newBuffer);
 };
